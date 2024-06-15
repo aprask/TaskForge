222dc20db3b47c32f871f034e1efb9e034755822
aprask <a.praskala@tutanota.com>
2024-06-15 17:40:09 -0400

    first commit

---
+++ backend/taskforge/src/main/java/com/api/taskforge/TaskForgeApplication.java
@@ -0,0 +1,13 @@
+package com.api.taskforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskForgeApplication.class, args);
+    }
+
+}
---
+++ backend/taskforge/src/test/java/com/api/taskforge/TaskForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.taskforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
